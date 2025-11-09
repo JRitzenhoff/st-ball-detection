d3b22792672f98425a55476e7888059c6488b9b3
Jan <jp.ritzenhoff@gmail.com>
2025-11-09 15:34:58 -0500

    Actually got the PWM input working

--- src/main.rs
+++ src/main.rs
@@ -3,54 +3,25 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Pull, Speed};
-use embassy_stm32::time::khz;
+use embassy_stm32::gpio::Pull;
+use embassy_stm32::time::Hertz;
 use embassy_stm32::timer::pwm_input::PwmInput;
-use embassy_stm32::{Peri, bind_interrupts, peripherals, timer};
+use embassy_stm32::{bind_interrupts, peripherals, timer};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
-
-/// Connect PB2 and PA6 with a 1k Ohm resistor
-
-#[embassy_executor::task]
-async fn blinky(led: Peri<'static, peripherals::PB2>) {
-    let mut led = Output::new(led, Level::High, Speed::Low);
-
-    let mut offset_counter: u64 = 0;
-    const SLEEP_DELTA: u64 = 50;
-    const DELTA_RESET: u64 = 10;
-
-    const WIDTH_RESET: u8 = 5;
-    let mut width_counter: u8 = 0;
-
-    loop {
-        info!("high");
-        led.set_high();
-        Timer::after_millis(300 + SLEEP_DELTA * offset_counter).await;
-
-        info!("low");
-        led.set_low();
-        Timer::after_millis(300).await;
-
-        width_counter = (width_counter + 1) % WIDTH_RESET;
-        if width_counter == 0 {
-            offset_counter = (offset_counter + 1) % DELTA_RESET;
-        }
-    }
-}
 
 bind_interrupts!(struct Irqs {
     TIM2 => timer::CaptureCompareInterruptHandler<peripherals::TIM2>;
 });
 
 #[embassy_executor::main]
-async fn main(spawner: Spawner) {
+async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
     info!("Hello World!");
 
-    let _ = spawner.spawn(blinky(p.PB2));
+    let sampling_rate = Hertz::khz(147);
 
-    let mut pwm_input = PwmInput::new_ch1(p.TIM3, p.PA6, Pull::None, khz(10));
+    let mut pwm_input = PwmInput::new_ch1(p.TIM3, p.PA6, Pull::None, sampling_rate);
     pwm_input.enable();
 
     loop {
